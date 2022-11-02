70e5dc607105911a341cd6802b537de4fa393721
Apoorve shukla <73848723+shukapurv@users.noreply.github.com>
2022-11-02 16:46:08 +0530

    Event page added

--- src/pages/AllEvents.tsx
+++ src/pages/AllEvents.tsx
@@ -5,11 +5,41 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { useAppDispatch } from "../utilities/hooks";
 import { useSelector } from "react-redux";
+import Button from "@mui/material/Button";
+
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import TextField from "@mui/material/TextField";
+
+const initialState = {
+  date: "",
+  time: "",
+  venue: "",
+  poster_url: "",
+  artist: "",
+  country: "",
+};
 
 const AllEvents = () => {
   const [events, setEvents] = useState([]);
   const dispatch = useAppDispatch();
 
+  const [open, setOpen] = React.useState(false);
+  const [val, setVal] = useState(initialState);
+  const handler = (e: any) => {
+    setVal({ ...val, [e.target.name]: e.target.value });
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     var user = JSON.parse(localStorage.getItem("user"));
@@ -28,6 +58,26 @@ const AllEvents = () => {
       });
   }, []);
 
+  const handlePost = () => {
+    handleClose();
+    var user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      user = user.token;
+    }
+    console.log(user);
+    axios
+      .post("http://localhost:8080/event/", {
+        param: val,
+        headers: {
+          Authorization: "Bearer " + user, //the token is a variable which holds the token
+        },
+      })
+      .then((response) => {
+        setEvents(response.data);
+        console.log(response.data);
+      });
+  };
+
   const state = {
     yellow: "#fcd000",
     blue: "#0ebeff",
@@ -36,6 +86,97 @@ const AllEvents = () => {
   };
   return (
     <div className="container">
+      <div>
+        <Button variant="outlined" onClick={handleClickOpen}>
+          Create New Event
+        </Button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          PaperProps={{
+            sx: {
+              width: "50%",
+              maxHeight: 600,
+            },
+          }}
+        >
+          <DialogTitle>Event</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Provide required data for new event
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="date"
+              label="Date"
+              name="date"
+              type="text"
+              onChange={handler}
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="time"
+              label="Time"
+              type="text"
+              name="time"
+              onChange={handler}
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="venue"
+              name="venue"
+              label="Venue"
+              type="text"
+              onChange={handler}
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="url"
+              name="poster_url"
+              label="Poster URL"
+              type="text"
+              onChange={handler}
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              name="artist"
+              id="name"
+              label="Artist Username"
+              type="email"
+              onChange={handler}
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="country"
+              label="Nationality"
+              type="email"
+              onChange={handler}
+              fullWidth
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handlePost}>Submit</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
       {events.map((event) => (
         <Card
           id={event.id}
