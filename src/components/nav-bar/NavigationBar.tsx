3b0ac2813a3bf09fae23df173723d5f59964e28b
Apoorve shukla <73848723+shukapurv@users.noreply.github.com>
2022-11-02 07:07:10 +0530

    nav update

--- src/components/nav-bar/NavigationBar.tsx
+++ src/components/nav-bar/NavigationBar.tsx
@@ -11,13 +11,14 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import { logout } from "../../feature/AuthSlice";
+import { logout, selectAuth } from "../../feature/AuthSlice";
 import { useAppDispatch } from "../../utilities/hooks";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useEffect } from "react";
 import GraphicEqIcon from "@mui/icons-material/GraphicEq";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const pages = ["Artists", "Playlists", "Search", "Albums"];
 const links = ["/artists/", "/playlist", "/search", "/album"];
@@ -35,16 +36,17 @@ function NavigationBar() {
     null
   );
 
+  const user = useSelector((state: any) => state.auth.username);
+
+  console.log(user);
 
   useEffect(() => {
-    const user = JSON.parse(localStorage.getItem("user") || "{}");
-    if (!user.token) {
-      setLoggedIn(false);
-    } else {
+    if (user) {
       setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
     }
-    console.log(isLoggerIn);
-  }, []);
+  }, [user]);
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -70,9 +72,9 @@ function NavigationBar() {
     toast.success("User logged out...");
   };
 
-  const logo=()=>{
-    if(JSON.parse(localStorage.getItem("user"))){
-      return JSON.parse(localStorage.getItem("user")).username
+  const logo = () => {
+    if (JSON.parse(localStorage.getItem("user"))) {
+      return JSON.parse(localStorage.getItem("user")).username;
     }
     return "?";
   };
@@ -170,44 +172,45 @@ function NavigationBar() {
               </Button>
             ))}
           </Box>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar
-                  alt={logo()}
-                  src="/static/images/avatar/2.jpg"
-                />
-                {""}
-                {/**Add username from store in alt*/}
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
+          {isLoggerIn ? (
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt={logo()} src="/static/images/avatar/2.jpg" />
+                  {""}
+                  {/**Add username from store in alt*/}
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+                <MenuItem key="logout" onClick={handleLogout}>
+                  <Typography textAlign="center">LogOut</Typography>
                 </MenuItem>
-              ))}
-              <MenuItem key="logout" onClick={handleLogout}>
-                <Typography textAlign="center">LogOut</Typography>
-              </MenuItem>
-            </Menu>
-            {/* <button onClick={handleLogout}>Logout</button> */}
-          </Box>
+              </Menu>
+              {/* <button onClick={handleLogout}>Logout</button> */}
+            </Box>
+          ) : (
+            ""
+          )}
         </Toolbar>
       </Container>
     </AppBar>
