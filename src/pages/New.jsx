3ee915e26e24aa6ae2f11466321d8b7a27145a00
shukapurv <apoorveshukla2001@gmail.com>
2022-11-04 03:05:13 +0530

    Addded new UI

---
+++ src/pages/New.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import Article from '../new/article/Article';
+import Brand from '../new/brand/Brand';
+import CTA from '../new/cta/CTA';
+import Navbar from '../new/navbar/Navbar';
+
+import Blog from '../containers/blog/Blog';
+import Features from '../containers/features/Features';
+import Footer from '../containers/footer/Footer';
+import Header from '../containers/header/Header';
+import Possibility from '../containers/possibility/Possibility';
+import WhatGPT3 from '../containers/whatGPT3/WhatGPT3';
+
+import "./new.css";
+
+const New = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Header />
+    </div>
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
+
+export default New;
