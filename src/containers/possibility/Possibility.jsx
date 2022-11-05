a9251fabef42e35945101fbe8c18612a05e5fe99
shukapurv <apoorveshukla2001@gmail.com>
2022-11-06 03:34:21 +0530

    change 1.2

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -5,19 +5,19 @@ import headphoneImage from '../../assets/headphone.png';
 import './possibility.css';
 
 const Possibility = () => (
-  <div className="gpt3__possibility section__padding" id="possibility">
+  <div className="gpt3__possibility section__padding" style={{paddingTop:"0vh", marginTop:"0vh"}} id="possibility">
     <div className="gpt3__possibility-image">
     <img  src={headphoneImage} alt="Headphone"></img>
       {/* <img src={possibilityImage} alt="possibility" /> */}
     </div>
     <div className="gpt3__possibility-content">
-      
-      
+
+
 
       <h1 className="gradient__text">The possibilities are <br /> beyond your imagination</h1>
-      
+
       <p> Every song is gonna hit you hard but we bet you won't feel the pain. Find and Enjoy your taste of music from the millions of songs available on our music web app. Nirvana - The Soul of Music, Music that touches you deep. Explore The Melody Within! </p>
-      
+
     </div>
   </div>
 );
