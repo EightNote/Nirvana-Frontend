02e933dcd0c528f8808a609f30042bec2b09afd1
Soumik Dutta <shalearkane@gmail.com>
2022-11-07 13:11:06 +0530

    added tracks component
    Signed-off-by: Soumik Dutta <shalearkane@gmail.com>

--- src/components/Album/AlbumDetails.jsx
+++ src/components/Album/AlbumDetails.jsx
@@ -1,42 +1,22 @@
 import * as React from "react";
-import EventImage from "../../assets/events.png"
 import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Routes,
   useParams,
 } from "react-router-dom";
-import { useEffect } from "react";
-import axios from "axios";
-import TrackCard from "../tracks/TrackCard";
+
+import Tracks from "../tracks/Tracks";
+import { useGetSpecificAlbumQuery } from "../../services/musicApi";
 
 function AlbumDetails() {
-    const { title } = useParams();
-    const [tracks, setTracks] = React.useState([]);
-    useEffect(() => {
-        var user = JSON.parse(localStorage.getItem("user"));
-        if (user) {
-            user = user.token;
-            console.log(user)
-        }
-        axios
-            .get("http://localhost:8080/albums/album/" + title, {
-                headers: {
-                    Authorization: "Bearer " + user, //the token is a variable which holds the token
-                },
-            })
-            .then((response) => {
-                console.log(response.data)
-                setTracks(response.data);
-            });
-    }, []);
+    const { id } = useParams();
+    const {data, isLoading, error} = useGetSpecificAlbumQuery(id)
+    if (isLoading) {
+        return (<p>Loading...</p>)
+    }
+    if (error) {
+        return (<p>Some error</p>)
+    }
     return (
-        <div>
-            {tracks.map((track)=> (
-                <TrackCard props={track}/>
-            ))}
-        </div>
+        <Tracks data={data}/>
     );
 }
 
---
+++ src/components/tracks/Tracks.jsx
@@ -0,0 +1,56 @@
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import LikeButton from '../Buttons/LikeButton';
+import PlayButton from '../Buttons/PlayButton'
+import { Box } from '@mui/system';
+
+export default function Tracks(data) {
+    // pass data as list of tracks
+    console.log(data)
+    return (
+        <Box sx={{
+            bgcolor: 'white',
+            boxShadow: 1,
+            borderRadius: 2,
+            p: 2,
+            minWidth: 300,
+          }}>
+        <List sx={{
+            width: '100vh',
+            height: '75vh',
+            overflowY: 'auto'
+        }}>
+            {data.data.map((track) => {
+                return (
+                    <ListItem>
+                        <ListItemButton role={undefined} dense
+                            sx={{
+                                backgroundColor: 'rgba(0, 0, 0, 0.1)',
+                                borderRadius: "20px",
+                            }}>
+                            <PlayButton trackid={track.title} edge="start" aria-label="play" />
+                            <ListItemText
+                                // className={styles.list}
+                                primary={track.title}
+                                // secondary={<div color='white'>{track.track.album.album_title}</div>}
+                                />
+                            {/* <ListItemText className={styles.list} >{track.album.album_title}</ListItemText>  */}
+
+                            <ListItemIcon>
+                                <LikeButton
+                                    trackid={track.id}
+                                    edge="end"
+                                />
+                                {/* <TrackActions/> */}
+                            </ListItemIcon>
+                        </ListItemButton>
+                    </ListItem>
+                );
+            })}
+        </List>
+        </Box>
+    );
+}
