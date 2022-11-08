790202f569e3eee32c1cc1ebe833b98a343bebc0
Soumik Dutta <shalearkane@gmail.com>
2022-11-09 00:51:40 +0530

    created add track component
    Signed-off-by: Soumik Dutta <shalearkane@gmail.com>

--- src/components/Playlist/PlaylistDetails.jsx
+++ src/components/Playlist/PlaylistDetails.jsx
@@ -1,26 +1,94 @@
-import { useGetSpecificPlaylistQuery, useGetSpecificPlaylistTracksQuery } from "../../services/musicApi";
-import Tracks from "../tracks/Tracks";
 import {
-    useParams,
-  } from "react-router-dom";
-  import { useSelector } from "react-redux";
+  useGetSpecificPlaylistQuery,
+  useGetSpecificPlaylistTracksQuery,
+  useGetTrackListQuery,
+} from "../../services/musicApi";
+import Tracks from "../tracks/Tracks";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import FormControl from "@mui/material/FormControl";
+import * as React from "react";
+import { Button, InputLabel } from "@mui/material";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import { Box } from "@mui/system";
+
+const AddTracksToPlaylist = () => {
+  const [track, setTrack] = React.useState(0);
+  const { data, isLoading, error } = useGetTrackListQuery();
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Some error</p>;
+  }
+  console.log(data);
+  const handleSubmit = () => {
+    console.log(track);
+  };
+  return (
+    <Box
+      sx={{
+        bgcolor: "white",
+        boxShadow: 1,
+        borderRadius: 2,
+        p: 2,
+        minWidth: 300,
+      }}
+    >
+      <FormControl required sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-helper-label">Song</InputLabel>
+        <Select
+          labelId="demo-simple-select-helper-label"
+          id="demo-simple-select-helper"
+          label="Visibility"
+          onChange={(event) => setTrack(event.target.value.title)}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          {data.map((track) => {
+            return <MenuItem value={track}>{track.title}</MenuItem>;
+          })}
+        </Select>
+      </FormControl>
+      <Button onClick={handleSubmit}>Add To Playlist</Button>
+    </Box>
+  );
+};
+
+const CheckIsOwnedByuser = (props) => {
+  const user = useSelector((state) => state.auth.username);
+  const { data, isLoading, error } = useGetSpecificPlaylistQuery(props.id);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Some error</p>;
+  }
+
+  if (data.createdByUser === user) {
+    return <AddTracksToPlaylist />;
+  } else {
+    return;
+  }
+};
 
 const PlaylistDetails = () => {
-    const { id } = useParams();
-    const user = useSelector((state) => state.auth.username);
-    const {dataP, isLoadingP, errorP} = useGetSpecificPlaylistQuery(id);
-    const {data, isLoading, error} =useGetSpecificPlaylistTracksQuery(id);
-    if (isLoading || isLoadingP) {
-        return (<p>Loading...</p>)
-    }
-    if (error || errorP) {
-        return (<p>Some error</p>)
-    }
-    return (
-        <div>
-                <Tracks data={data}/>
-        </div>
-    );
-}
+  const { id } = useParams();
+  const { data, isLoading, error } = useGetSpecificPlaylistTracksQuery(id);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Some error</p>;
+  }
+  return (
+    <div>
+      <CheckIsOwnedByuser id={id} />
+      <Tracks data={data} />
+    </div>
+  );
+};
 
 export default PlaylistDetails;
