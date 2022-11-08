71c7a12798292879a704f3a22466f67e95ddd1c6
vivekKr24 <vivek.kumar.cse20@itbhu.ac.in>
2022-11-09 04:14:52 +0530

    User page update

--- src/user_components/Sidebar/Data.jsx
+++ src/user_components/Sidebar/Data.jsx
@@ -1,5 +1,9 @@
 import { Box, Text, VStack } from '@chakra-ui/react'
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { Button } from '@chakra-ui/react'
+import { Badge } from '@chakra-ui/react'
 
 const list = [
   {
@@ -34,7 +38,39 @@ const list = [
   }
 ]
 
+
+const GenreTag = (props) => {
+  return (
+    <a >
+      <Button style={{ margin: "10px 5px"}} size="xs" colorScheme='teal' variant='outline'>
+        {props.genre_name}
+        <Badge ml='1' fontSize='0.8em' colorScheme='green'>
+          {props.track_count}
+        </Badge>
+      </Button>
+    </a>
+  )
+}
+
+// const GenreList = (props) => {
+//   return (
+
+//   )
+// }
+
 function Data() {
+  const [interests, setInterests] = useState([{name: "Punjabi", "track_count":22, "id": 1}])
+  const username = useSelector((state) => state.auth.username)
+  const role = useSelector((state) => state.auth.role)
+  const user = JSON.parse(localStorage.getItem("user"))
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/user/get-user-detail/' + user.username).then((res) => {
+        console.log("User details", res.data)
+        setInterests(res.data.interests)
+    }).catch((error) => console.log(error))
+  }, [])
+
   return (
     <VStack as="ul" spacing={0} listStyleType="none">
       {list.map(item => (
@@ -56,6 +92,12 @@ function Data() {
           </Text>
         </Box>
       ))}
+      {(role == "user") &&  
+        <div style={{ display:"flex", flexDirection:"row", flexWrap:"wrap" }}>
+          {interests.map((genre) => <GenreTag id = {genre.id} track_count = {genre.track_count} genre_name={genre.name}/>)}
+        </div>
+      }
+
     </VStack>
   )
 }
