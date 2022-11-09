b12bba668ef391e044fefc0dbfeacbb119378902
Soumik Dutta <shalearkane@gmail.com>
2022-11-09 08:06:41 +0530

    added button to like albums
    Signed-off-by: Soumik Dutta <shalearkane@gmail.com>

--- src/components/Album/AlbumDetails.jsx
+++ src/components/Album/AlbumDetails.jsx
@@ -1,12 +1,16 @@
 import * as React from "react";
 import { useParams } from "react-router-dom";
-
+import { useState, useEffect, skipToken } from "react";
+import axios from "axios";
 import Tracks from "../tracks/Tracks";
-import { useGetSpecificAlbumQuery } from "../../services/musicApi";
+import { useGetSpecificAlbumQuery, useIsAlbumLikedQuery, useToggleLikeAlbumMutation } from "../../services/musicApi";
 import { useSelector } from "react-redux";
 import CreateTrack from "../tracks/CreateTrack";
 import { useGetAlbumDetailsQuery } from "../../services/musicApi";
 import { Box } from "@mui/system";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import IconButton from "@mui/material/IconButton";
 
 const CheckIsArtist = (props) => {
   const user = useSelector((state) => state.auth.username);
@@ -20,17 +24,62 @@ const CheckIsArtist = (props) => {
   }
 };
 
+const AlbumLike = (props) => {
+  const [like, setLike] = useState(skipToken);
+  useEffect(() => {
+    axios.get("http://localhost:8080/albums/is-liked-by/" + props.id).then((res) => {
+      setLike(res.data);
+    });
+    console.log(props.id);
+  }, []);
+
+  const [toggleLike, resultLike] = useToggleLikeAlbumMutation();
+  const { data: likeStatus, isLoading, isError } = useIsAlbumLikedQuery(props.id);
+
+  useEffect(() => {
+    console.log(props.id + " " + likeStatus);
+    if (likeStatus) {
+      setLike(true);
+    }
+  }, [likeStatus, props]);
+
+  function likeHandler() {
+    if (like === false) {
+      setLike(true);
+    } else {
+      setLike(false);
+    }
+    toggleLike(props.id);
+  }
+
+  return (
+    <IconButton aria-label="like" onClick={likeHandler}>
+      {like ? <FavoriteIcon style={{color:"white"}} /> : <FavoriteBorderOutlinedIcon style={{color:"white"}} />}
+    </IconButton>
+  );
+};
+
 const AlbumHeader = (props) => {
   const { data, isLoading, error } = useGetAlbumDetailsQuery(props.id);
   if (isLoading) return;
   if (error) return;
   return (
-    <div>
+    <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          p: 1,
+          m: 1,
+          bgcolor: 'background.paper',
+          borderRadius: 1,
+        }}>
       <p>
         <h1 style={{ color: "white" }}> {data.album_title}</h1>
         <h5 style={{ color: "orange" }}>{data.artist_id}</h5>
       </p>
-    </div>
+
+        <AlbumLike id={props.id} />
+    </Box>
   );
 };
 
